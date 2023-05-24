84a930448c32c0e6f4067f47c0f2c05ae7a7bfa2
ivanrdz2001 <ivan.rodriguezm@uanl.edu.mx>
2023-05-23 19:31:49 -0600

    video funciona?

--- ProcesamientoCorrecto/ProcesamientoCorrecto/Form4.Designer.cs
+++ ProcesamientoCorrecto/ProcesamientoCorrecto/Form4.Designer.cs
@@ -29,8 +29,10 @@ namespace ProcesamientoCorrecto
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form4));
             this.panel1 = new System.Windows.Forms.Panel();
+            this.playVideo = new System.Windows.Forms.Button();
             this.Salir = new System.Windows.Forms.Button();
             this.loadImagePictureForm = new System.Windows.Forms.Button();
             this.panel2 = new System.Windows.Forms.Panel();
@@ -40,23 +42,48 @@ namespace ProcesamientoCorrecto
             this.button2 = new System.Windows.Forms.Button();
             this.comboEfectosImagen = new System.Windows.Forms.ComboBox();
             this.fileSystemWatcher1 = new System.IO.FileSystemWatcher();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.histogramaOriginal = new System.Windows.Forms.PictureBox();
+            this.reestablecerVideo = new System.Windows.Forms.Button();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.histogramaEditado = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
             this.panel1.SuspendLayout();
             this.panel2.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.resetMainButton)).BeginInit();
             this.groupBox2.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.histogramaOriginal)).BeginInit();
+            this.groupBox3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.histogramaEditado)).BeginInit();
             this.SuspendLayout();
             // 
             // panel1
             // 
             this.panel1.BackColor = System.Drawing.Color.Gold;
+            this.panel1.Controls.Add(this.playVideo);
             this.panel1.Controls.Add(this.Salir);
             this.panel1.Controls.Add(this.loadImagePictureForm);
             this.panel1.Location = new System.Drawing.Point(0, 109);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(207, 505);
             this.panel1.TabIndex = 0;
+            // 
+            // playVideo
+            // 
+            this.playVideo.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.playVideo.Font = new System.Drawing.Font("Montserrat Black", 13F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.playVideo.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.playVideo.Location = new System.Drawing.Point(46, 222);
+            this.playVideo.Name = "playVideo";
+            this.playVideo.Size = new System.Drawing.Size(108, 90);
+            this.playVideo.TabIndex = 15;
+            this.playVideo.Text = "Play";
+            this.playVideo.UseVisualStyleBackColor = false;
+            this.playVideo.Click += new System.EventHandler(this.playVideo_Click);
             // 
             // Salir
             // 
@@ -83,6 +110,7 @@ namespace ProcesamientoCorrecto
             this.loadImagePictureForm.TabIndex = 4;
             this.loadImagePictureForm.Text = "Cargar video";
             this.loadImagePictureForm.UseVisualStyleBackColor = false;
+            this.loadImagePictureForm.Click += new System.EventHandler(this.loadImagePictureForm_Click);
             // 
             // panel2
             // 
@@ -139,6 +167,7 @@ namespace ProcesamientoCorrecto
             this.button2.TabIndex = 13;
             this.button2.Text = "Guardar video";
             this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // comboEfectosImagen
             // 
@@ -157,6 +186,61 @@ namespace ProcesamientoCorrecto
             this.fileSystemWatcher1.EnableRaisingEvents = true;
             this.fileSystemWatcher1.SynchronizingObject = this;
             // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.Transparent;
+            this.groupBox1.Controls.Add(this.histogramaOriginal);
+            this.groupBox1.Location = new System.Drawing.Point(895, 31);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(313, 203);
+            this.groupBox1.TabIndex = 14;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Histograma original";
+            // 
+            // histogramaOriginal
+            // 
+            this.histogramaOriginal.Location = new System.Drawing.Point(15, 25);
+            this.histogramaOriginal.Name = "histogramaOriginal";
+            this.histogramaOriginal.Size = new System.Drawing.Size(271, 166);
+            this.histogramaOriginal.TabIndex = 0;
+            this.histogramaOriginal.TabStop = false;
+            // 
+            // reestablecerVideo
+            // 
+            this.reestablecerVideo.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.reestablecerVideo.Font = new System.Drawing.Font("Montserrat Black", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.reestablecerVideo.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.reestablecerVideo.Location = new System.Drawing.Point(395, 527);
+            this.reestablecerVideo.Name = "reestablecerVideo";
+            this.reestablecerVideo.Size = new System.Drawing.Size(359, 49);
+            this.reestablecerVideo.TabIndex = 15;
+            this.reestablecerVideo.Text = "Reestablecer";
+            this.reestablecerVideo.UseVisualStyleBackColor = false;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.BackColor = System.Drawing.Color.Transparent;
+            this.groupBox3.Controls.Add(this.histogramaEditado);
+            this.groupBox3.Location = new System.Drawing.Point(895, 264);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(313, 225);
+            this.groupBox3.TabIndex = 16;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Histograma editado";
+            // 
+            // histogramaEditado
+            // 
+            this.histogramaEditado.Location = new System.Drawing.Point(22, 22);
+            this.histogramaEditado.Name = "histogramaEditado";
+            this.histogramaEditado.Size = new System.Drawing.Size(264, 188);
+            this.histogramaEditado.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.histogramaEditado.TabIndex = 0;
+            this.histogramaEditado.TabStop = false;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
             // Form4
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
@@ -164,8 +248,11 @@ namespace ProcesamientoCorrecto
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(253)))), ((int)(((byte)(202)))), ((int)(((byte)(64)))));
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.ClientSize = new System.Drawing.Size(903, 532);
+            this.ClientSize = new System.Drawing.Size(1240, 582);
             this.ControlBox = false;
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.reestablecerVideo);
+            this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.comboEfectosImagen);
@@ -180,6 +267,11 @@ namespace ProcesamientoCorrecto
             this.groupBox2.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.histogramaOriginal)).EndInit();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.histogramaEditado)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -196,5 +288,13 @@ namespace ProcesamientoCorrecto
         private System.Windows.Forms.ComboBox comboEfectosImagen;
         private System.IO.FileSystemWatcher fileSystemWatcher1;
         private System.Windows.Forms.Button Salir;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.PictureBox histogramaEditado;
+        private System.Windows.Forms.Button reestablecerVideo;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.PictureBox histogramaOriginal;
+        private System.Windows.Forms.Button playVideo;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
     }
 }
--- ProcesamientoCorrecto/ProcesamientoCorrecto/Form4.cs
+++ ProcesamientoCorrecto/ProcesamientoCorrecto/Form4.cs
@@ -1,36 +1,62 @@
-﻿using Emgu.CV;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
-using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Emgu.CV;
+using Emgu.CV.Structure;
+using Emgu.CV.UI;
+using Emgu.CV.CvEnum;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+using OpenTK.Graphics.OpenGL;
 
 namespace ProcesamientoCorrecto
 {
     public partial class Form4 : Form
     {
-        int index = 0;
+        Bitmap newFile;
+        Bitmap originalFile;
+        VideoCapture videoCapture;
+        bool isPlaying = false;
+        int activeFilter = -1;
+        int totalFrames;
+        int currentFrameNumber;
+        int fps;
+        String fileName;
 
-        private string ruta = "";
+        Mat currentFrame = new Mat();
+        List<Bitmap> arrayBitmaps;
+        //ImageManipulation modifyRGB = new ImageManipulation();
+        //FileOperations getFile = new FileOperations();
 
-        private int[,] conv3x3 = new int[3, 3];
-        private int[,] conv5x5 = new int[5, 5];
 
-        private int factor;
-        private int offset;
 
-        private int filterSelection = 0;
 
 
         public Form4()
         {
             InitializeComponent();
+            System.Object[] ItemObject = new System.Object[8];
+
+            ItemObject[0] = "NEGATIVO";
+            ItemObject[1] = "BLANCO/NEGRO";
+            ItemObject[2] = "DESENFOQUE";
+            ItemObject[3] = "BORDES GRISES";
+            ItemObject[4] = "SEPIA";
+            ItemObject[5] = "ABERRACION CROMATICA";
+            ItemObject[6] = "PIXEL";
+            ItemObject[7] = "MEXICO EN BREAKING BAD";
+
+
+
         }
+
+
 
         private void resetMainButton_Click(object sender, EventArgs e)
         {
@@ -56,6 +82,135 @@ namespace ProcesamientoCorrecto
         private void Form4_Load(object sender, EventArgs e)
         {
 
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void playVideo_Click(object sender, EventArgs e)
+        {
+            if (index == 0)
+            {
+                playVideo.Text = "Play";
+                timer1.Stop();
+                index = 1;
+
+            }
+            else if (index != 0)
+            {
+                playVideo.Text = "Pausa";
+                timer1.Start();
+                index = 0;
+
+            }
+        }
+
+        public string openVideoFile()
+        {
+
+            OpenFileDialog ofd = new OpenFileDialog();
+
+            ofd.InitialDirectory = "C:\\Downloads";
+            ofd.Filter = "videos| *.mp4; *.mkv; *.mov";
+
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                fileName = ofd.FileName;
+            }
+
+            return fileName;
+        }
+
+        private void loadImagePictureForm_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = false;
+            fileName = openVideoFile();
+
+            if (fileName != null)
+            {
+                videoCapture = new VideoCapture(fileName);
+                totalFrames = Convert.ToInt32(videoCapture.Get(Emgu.CV.CvEnum.CapProp.FrameCount));
+                fps = Convert.ToInt32(videoCapture.Get(Emgu.CV.CvEnum.CapProp.Fps));
+                currentFrame = new Mat();
+                currentFrameNumber = 0;
+                comboEfectosImagen.Enabled = true;
+                reestablecerVideo.Enabled = true;
+                playVideo.BackColor = Color.FromArgb(210, 150, 57);
+
+                PlayVideo();
+            }
+            if (fileName != null)
+            {
+
+                comboEfectosImagen.Enabled = true;
+                reestablecerVideo.Enabled = true;
+            }
+        }
+
+        private async void PlayVideo()
+        {
+            if (videoCapture != null)
+            {
+                isPlaying = true;
+                playVideo.Enabled = true;
+                try
+                {
+                    playVideo.BackColor = Color.FromArgb(210, 150, 57);
+
+
+                    while (isPlaying && currentFrameNumber < totalFrames)
+                    {
+
+                        chartHistOriginal.Series["Red"].Points.Clear();
+                        chartHistOriginal.Series["Green"].Points.Clear();
+                        chartHistOriginal.Series["Blue"].Points.Clear();
+
+                        chartHistEdited.Series["Red"].Points.Clear();
+                        chartHistEdited.Series["Green"].Points.Clear();
+                        chartHistEdited.Series["Blue"].Points.Clear();
+
+                        videoCapture.Read(currentFrame);
+
+                        newFile = currentFrame.ToBitmap();
+
+                        originalFile = new Bitmap(newFile);
+
+                        DisplayImage(originalFile, 1);
+                        DisplayImage(newFile, 2);
+
+                        modifyRGB.getLockBitsHistogram(originalFile, 'r', 1);
+                        modifyRGB.getLockBitsHistogram(originalFile, 'b', 1);
+                        modifyRGB.getLockBitsHistogram(originalFile, 'g', 1);
+
+                        if (activeFilter >= 0)
+                            modifyRGB.ManipulateLockBits(newFile, activeFilter + 1, tbSliderControl.Value);
+
+
+                        currentFrameNumber++;
+
+                        await Task.Delay(1000 / fps);
+
+                    }
+
+                    playVideo.BackColor = Color.FromArgb(10, 13, 39);
+
+
+                    if (currentFrameNumber >= totalFrames)
+                    {
+                        currentFrameNumber = 0;
+                        isPlaying = false;
+                    }
+
+
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+
+            }
         }
     }
 }
